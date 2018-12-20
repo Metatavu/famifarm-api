c397d8418507357c703d2c2c0fb924f47a673630
Ville Koivukangas <ville.koivukangas12@gmail.com>
2018-12-20 10:03:36 +0200

    travis

--- travis.sh
+++ travis.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-export MAVEN_OPTS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ $TRAVIS_BRANCH == "master" ]; then
   echo "Release"
 else 
-  mvn clean verify jacoco:report coveralls:report -Pitests sonar:sonar -DrepoToken=$COVERALLS_TOKEN
+  mvn clean verify jacoco:report coveralls:report -Pitests sonar:sonar -DrepoToken=$COVERALLS_TOKEN -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
 fi
