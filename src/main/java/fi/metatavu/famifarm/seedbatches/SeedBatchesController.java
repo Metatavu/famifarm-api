1c8309c3dc9f6b042381622881a74b5654580ba4
Ville Koivukangas <ville.koivukangas12@gmail.com>
2019-01-29 10:51:50 +0200

    code clean

--- src/main/java/fi/metatavu/famifarm/seedbatches/SeedBatchesController.java
+++ src/main/java/fi/metatavu/famifarm/seedbatches/SeedBatchesController.java
@@ -18,67 +18,67 @@ import fi.metatavu.famifarm.persistence.model.SeedBatch;
  */
 public class SeedBatchesController {
 
-	@Inject
-	private SeedBatchDAO seedBatchDAO;
+  @Inject
+  private SeedBatchDAO seedBatchDAO;
 
-	/**
-	 * Creates new seed batch
-	 * 
-	 * @param code code
-	 * @param seed seed
-	 * @param time time
-	 * @param userId userId
-	 * @return created seed batch
-	 */
-	public SeedBatch createSeedBatch(String code, Seed seed, OffsetDateTime time, UUID userId) {
-		return seedBatchDAO.create(UUID.randomUUID(), code, seed, time, userId, userId);
-	}
+  /**
+   * Creates new seed batch
+   * 
+   * @param code code
+   * @param seed seed
+   * @param time time
+   * @param userId userId
+   * @return created seed batch
+   */
+  public SeedBatch createSeedBatch(String code, Seed seed, OffsetDateTime time, UUID userId) {
+    return seedBatchDAO.create(UUID.randomUUID(), code, seed, time, userId, userId);
+  }
 
-	/**
-	 * Finds seed batch by id
-	 * 
-	 * @param seedBatchId seed batch id
-	 * @return seed batch
-	 */
-	public SeedBatch findSeedBatch(UUID seedBatchId) {
-		return seedBatchDAO.findById(seedBatchId);
-	}
+  /**
+   * Finds seed batch by id
+   * 
+   * @param seedBatchId seed batch id
+   * @return seed batch
+   */
+  public SeedBatch findSeedBatch(UUID seedBatchId) {
+    return seedBatchDAO.findById(seedBatchId);
+  }
 
-	/**
-	 * Lists seeds batches
-	 * 
-	 * @param firstResult first result
-	 * @param maxResults max results
-	 * @return list of seed batches
-	 */
-	public List<SeedBatch> listSeedBatches(Integer firstResult, Integer maxResults) {
-		return seedBatchDAO.listAll(firstResult, maxResults);
-	}
+  /**
+   * Lists seeds batches
+   * 
+   * @param firstResult first result
+   * @param maxResults max results
+   * @return list of seed batches
+   */
+  public List<SeedBatch> listSeedBatches(Integer firstResult, Integer maxResults) {
+    return seedBatchDAO.listAll(firstResult, maxResults);
+  }
 
-	/**
-	 * Creates new seed batch
-	 * 
-	 * @param seedBatch seedBatch
-	 * @param code code
-	 * @param seed seed
-	 * @param time time
-	 * @param lastModifierId lastModifierId
-	 * @return created seed batch
-	 */
-	public SeedBatch updateSeedBatch(SeedBatch seedBatch, String code, Seed seed, OffsetDateTime time, UUID lastModifierId) {
-		seedBatchDAO.updateCode(seedBatch, code, lastModifierId);
-		seedBatchDAO.updateSeed(seedBatch, seed, lastModifierId);
-		seedBatchDAO.updateTime(seedBatch, time, lastModifierId);
-		return seedBatch;
-	}
+  /**
+   * Creates new seed batch
+   * 
+   * @param seedBatch seedBatch
+   * @param code code
+   * @param seed seed
+   * @param time time
+   * @param lastModifierId lastModifierId
+   * @return created seed batch
+   */
+  public SeedBatch updateSeedBatch(SeedBatch seedBatch, String code, Seed seed, OffsetDateTime time, UUID lastModifierId) {
+    seedBatchDAO.updateCode(seedBatch, code, lastModifierId);
+    seedBatchDAO.updateSeed(seedBatch, seed, lastModifierId);
+    seedBatchDAO.updateTime(seedBatch, time, lastModifierId);
+    return seedBatch;
+  }
 
-	/**
-	 * Deletes a seed batch
-	 * 
-	 * @param seedBatch seedBatch to be deleted
-	 */
-	public void deleteSeedBatch(SeedBatch seedBatch) {
-		seedBatchDAO.delete(seedBatch);
-	}
+  /**
+   * Deletes a seed batch
+   * 
+   * @param seedBatch seedBatch to be deleted
+   */
+  public void deleteSeedBatch(SeedBatch seedBatch) {
+    seedBatchDAO.delete(seedBatch);
+  }
 
 }
