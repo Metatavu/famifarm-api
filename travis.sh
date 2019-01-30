da90825f9efed677092f0cb13534c2a394a1160e
Antti Leppä <antti.leppa@metatavu.fi>
2019-01-30 20:30:25 +0200

    Travis

--- travis.sh
+++ travis.sh
@@ -6,6 +6,7 @@ else
   echo "[mysqld]" > $HOME/.my.cnf
   echo "lower-case-table-names = 1" >> $HOME/.my.cnf
   sudo service mysql restart
+  mysql -u root -p -e "show variables like 'lower%'"
   mysql -u root < scripts/setup-db.sql
   mvn clean verify jacoco:report coveralls:report -Pitests sonar:sonar -DrepoToken=$COVERALLS_TOKEN --batch-mode -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
 fi
