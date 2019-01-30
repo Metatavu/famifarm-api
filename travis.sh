a3966a3ae341a98bc3ccc423f75b63e2bd52bf11
Antti Leppä <antti.leppa@metatavu.fi>
2019-01-30 20:36:17 +0200

    Travis

--- travis.sh
+++ travis.sh
@@ -3,10 +3,5 @@ export MAVEN_OPTS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ $TRAVIS_BRANCH == "master" ]; then
   echo "Release"
 else 
-  echo "[mysqld]" > $HOME/.my.cnf
-  echo "lower-case-table-names = 1" >> $HOME/.my.cnf
-  sudo service mysql restart
-  mysql -u root -p -e "show variables like 'lower%'"
-  mysql -u root < scripts/setup-db.sql
   mvn clean verify jacoco:report coveralls:report -Pitests sonar:sonar -DrepoToken=$COVERALLS_TOKEN --batch-mode -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
 fi
