3271d8878b3b661d30b867459473e9569f74ca16
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2020-04-01 12:30:04 +0300

    Fixed yield reports

--- src/main/java/fi/metatavu/famifarm/reporting/xlsx/XlsxPlantingYieldReport.java
+++ src/main/java/fi/metatavu/famifarm/reporting/xlsx/XlsxPlantingYieldReport.java
@@ -1,28 +1,25 @@
 package fi.metatavu.famifarm.reporting.xlsx;
 
 import java.io.OutputStream;
-import java.time.OffsetDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.UUID;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
-import fi.metatavu.famifarm.batches.BatchController;
 import fi.metatavu.famifarm.events.EventController;
 import fi.metatavu.famifarm.localization.LocalesController;
 import fi.metatavu.famifarm.localization.LocalizedValueController;
-import fi.metatavu.famifarm.persistence.model.Batch;
 import fi.metatavu.famifarm.persistence.model.Event;
-import fi.metatavu.famifarm.persistence.model.HarvestEvent;
-import fi.metatavu.famifarm.persistence.model.PlantingEvent;
 import fi.metatavu.famifarm.persistence.model.Product;
-import fi.metatavu.famifarm.persistence.model.SowingEvent;
+import fi.metatavu.famifarm.reporting.EventCountController;
 import fi.metatavu.famifarm.reporting.ReportException;
-import fi.metatavu.famifarm.rest.model.PotType;
 import fi.metatavu.famifarm.rest.model.EventType;
 
 /**
@@ -32,29 +29,28 @@ import fi.metatavu.famifarm.rest.model.EventType;
  */
 @ApplicationScoped
 public class XlsxPlantingYieldReport extends AbstractXlsxReport {
+
   @Inject
   private LocalesController localesController;
-  
+
   @Inject
   private EventController eventController;
-  
-  @Inject
-  private BatchController batchController;
-  
+
   @Inject
   private LocalizedValueController localizedValueController;
+
+  @Inject
+  private EventCountController eventCountController;
 
   @Override
   public void createReport(OutputStream output, Locale locale, Map<String, String> parameters) throws ReportException {
     try (XlsxBuilder xlsxBuilder = new XlsxBuilder()) {
       String sheetId = xlsxBuilder.createSheet(localesController.getString(locale, "reports.plantingYield.plantingYieldTitle"));
-      
-      int teamIndex = 0;
-      int productIndex = 1;
-      int dateIndex = 2;
-      int inGuttersIndex = 3;
-      int fromCellsIndex = 4;
-      int yieldIndex = 5;
+
+      int productIndex = 0;
+      int inGuttersIndex = 1;
+      int fromCellsIndex = 2;
+      int yieldIndex = 3;
       
       Date fromTime = Date.from(parseDate(parameters.get("fromTime")).toInstant());
       Date toTime = Date.from(parseDate(parameters.get("toTime")).toInstant());
@@ -63,35 +59,41 @@ public class XlsxPlantingYieldReport extends AbstractXlsxReport {
       
       xlsxBuilder.setCellValue(sheetId, 0, 0, localesController.getString(locale, "reports.plantingYield.plantingYieldTitle")); 
       xlsxBuilder.setCellValue(sheetId, 1, 0, localesController.getString(locale, "reports.common.dateBetween", fromTime, toTime)); 
-      xlsxBuilder.setCellValue(sheetId, 3, teamIndex, localesController.getString(locale, "reports.plantingYield.plantingYieldTeam")); 
       xlsxBuilder.setCellValue(sheetId, 3, productIndex, localesController.getString(locale, "reports.plantingYield.plantingYieldProduct"));
-      xlsxBuilder.setCellValue(sheetId, 3, dateIndex, localesController.getString(locale, "reports.plantingYield.plantingYieldDate"));
       xlsxBuilder.setCellValue(sheetId, 3, inGuttersIndex, localesController.getString(locale, "reports.plantingYield.plantingYieldInGutters"));
       xlsxBuilder.setCellValue(sheetId, 3, fromCellsIndex, localesController.getString(locale, "reports.plantingYield.plantingYieldFromCells")); 
       xlsxBuilder.setCellValue(sheetId, 3, yieldIndex, localesController.getString(locale, "reports.plantingYield.plantingYieldPercentage"));
       
       // Values
-      
-      List<Batch> batches = batchController.listBatches(null, null, null, null, null, parseDate(parameters.get("toTime")), parseDate(parameters.get("fromTime")), null, null);
-      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+      List<Event> events = eventController.listByStartTimeAfterAndStartTimeBefore(parseDate(parameters.get("toTime")), parseDate(parameters.get("fromTime")));
+      Map<UUID, ReportRow> rowLookup = new HashMap<>();
+      events.stream().forEach(event -> {
+        Product product = event.getBatch().getProduct();
+        if (!rowLookup.containsKey(product.getId())) {
+          rowLookup.put(
+            product.getId(),
+            new ReportRow(localizedValueController.getValue(product.getName(), locale),
+            eventCountController.countUnitsByProductAndEventType(events, product, EventType.TABLE_SPREAD),
+            eventCountController.countUnitsByProductAndEventType(events, product, EventType.PLANTING)
+          ));
+        }
+      });
+
       int rowIndex = 4;
-      
-      for (Batch batch : batches) {
-        List<Event> events = eventController.listByBatchSortByStartTimeAsc(batch, null, null); 
-        
-        Product product = batch.getProduct();
-        String dateString = getDateString(events, formatter);
-        String team = getTeam(events, locale);
-        int totalPlantedAmount = getTotalPlantedAmount(events);
-        int totalAmountInGutters = getTotalAmounInGutters(events);
-        long yield = getYield(totalPlantedAmount, totalAmountInGutters);
-        
-        xlsxBuilder.setCellValue(sheetId, rowIndex, teamIndex, team);
-        xlsxBuilder.setCellValue(sheetId, rowIndex, productIndex, localizedValueController.getValue(product.getName(), locale));
-        xlsxBuilder.setCellValue(sheetId, rowIndex, dateIndex, dateString);
-        xlsxBuilder.setCellValue(sheetId, rowIndex, inGuttersIndex, totalAmountInGutters);
-        xlsxBuilder.setCellValue(sheetId, rowIndex, fromCellsIndex, totalPlantedAmount);
-        xlsxBuilder.setCellValue(sheetId, rowIndex, yieldIndex,  yield);
+      List<ReportRow> rows = new ArrayList<>(rowLookup.values());
+      Collections.sort(rows);
+
+      for (ReportRow row : rows) {
+
+        Double totalPlantedAmount = row.getSpreadAmount();
+        Double totalAmountInGutters = row.getAmountInGutters();
+        Double yield = getYield(totalPlantedAmount, totalAmountInGutters);
+
+        xlsxBuilder.setCellValue(sheetId, rowIndex, productIndex, row.getProductName());
+        xlsxBuilder.setCellValue(sheetId, rowIndex, inGuttersIndex, Double.toString(totalAmountInGutters));
+        xlsxBuilder.setCellValue(sheetId, rowIndex, fromCellsIndex, Double.toString(totalPlantedAmount));
+        xlsxBuilder.setCellValue(sheetId, rowIndex, yieldIndex,  Double.toString(yield));
         rowIndex++;
       }
       
@@ -102,120 +104,65 @@ public class XlsxPlantingYieldReport extends AbstractXlsxReport {
   }
   
   /**
-   * Get date
-   * 
-   * @param events events
-   * @param formatter formatter
-   * @return date string
-   */
-  private String getDateString(List<Event> events, DateTimeFormatter formatter) {
-    OffsetDateTime date = null;
-    
-    for (Event event : events) {
-      if (event.getType() == EventType.PLANTING) {
-        PlantingEvent plantingEvent = (PlantingEvent) event;
-        if (plantingEvent.getEndTime() != null) {
-          date = plantingEvent.getEndTime();
-        }
-      }
-    }
-    
-    if (date != null) {
-      return date.format(formatter);
-    }
-    
-    return "";
-  }
-  
-  /**
-   * Get total sowed amount
-   * 
-   * @param events
-   * @return total sowed amount
-   */
-  private int getTotalPlantedAmount(List<Event> events) {
-    int amount = 0;
-    
-    SowingEvent sowingEvent = (SowingEvent) events.stream().filter(event -> event.getType() == EventType.SOWING).findFirst().orElse(null);
-    if (sowingEvent == null) {
-      return 0;
-    }
-    
-    PotType potType = sowingEvent.getPotType();
-
-    for (Event event : events) {
-      if (event.getType() == EventType.PLANTING) {
-        PlantingEvent plantingEvent = (PlantingEvent) event;
-        amount += plantingEvent.getTrayCount() * getPotTypeAmount(potType);
-      }
-    }
-    
-    return amount;
-  }
-  
-  /**
-   * Get cell type as int
-   * 
-   * @param potType, potType
-   * @return amount
-   */
-  private int getPotTypeAmount(PotType potType) {
-    if (PotType.SMALL == potType) {
-      return 54;
-    }
-    return 35;
-  }
-  
-  /**
-   * Get total amount in gutters
-   * 
-   * @param events events
-   * @return total amount in gutters
-   */
-  private int getTotalAmounInGutters(List<Event> events) {
-    int amount = 0;
-    
-    for (Event event : events) {
-      if (event.getType() == EventType.PLANTING) {
-        PlantingEvent plantingEvent = (PlantingEvent) event;
-        amount += plantingEvent.getGutterHoleCount() * plantingEvent.getGutterCount();
-      }
-    }
-    
-    return amount;
-  }
-  
-  /**
    * Get yield percentage
    * 
    * @param totalPlantedAmount totalSowedAmount
    * @param totalAmountInGutters totalAmountInGutters
    * @return yield
    */
-  private long getYield(int totalPlantedAmount, int totalAmountInGutters) {
-    if (totalAmountInGutters == 0) {
-      return 0l;
+  private double getYield(Double totalPlantedAmount, Double totalAmountInGutters) {
+    if (totalAmountInGutters == 0 || totalPlantedAmount == 0) {
+      return 0d;
     }
     
     return (totalAmountInGutters * 100) / totalPlantedAmount; 
   }
-  
+
   /**
-   * Get team
-   * 
-   * @param events events
-   * @return team
+   * Inner class representing single row in report
    */
-  private String getTeam(List<Event> events, Locale locale) {
-    for (Event event : events) {
-      if (event.getType() == EventType.HARVEST) {
-        HarvestEvent harvestEvent = (HarvestEvent) event;
-        if (harvestEvent.getProductionLine().getDefaultTeam() != null) {
-          return localizedValueController.getValue(harvestEvent.getProductionLine().getDefaultTeam().getName(), locale);
-        }
-        break;
-      }
+  private class ReportRow implements Comparable<ReportRow>{
+
+    public ReportRow(String productName, Double spreadAmount, Double amountInGutters) {
+      this.productName = productName;
+      this.spreadAmount = spreadAmount;
+      this.amountInGutters = amountInGutters;
     }
-    return "";
+
+    private Double spreadAmount;
+
+    private Double amountInGutters;
+
+    private String productName;
+
+    /**
+     * @return the productName
+     */
+    public String getProductName() {
+      return productName;
+    }
+
+    @Override
+    public int compareTo(ReportRow other) {
+      return this.productName.compareToIgnoreCase(other.getProductName());
+    }
+
+    public Double getSpreadAmount() {
+      return spreadAmount;
+    }
+
+    public void setSpreadAmount(Double spreadAmount) {
+      this.spreadAmount = spreadAmount;
+    }
+
+    public Double getAmountInGutters() {
+      return amountInGutters;
+    }
+
+    public void setAmountInGutters(Double amountInGutters) {
+      this.amountInGutters = amountInGutters;
+    }
+
   }
+
 }
